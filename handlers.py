da724e0bfbd89994fa808a59c57700509785585a
saslv <yar.hasko@gmail.com>
2015-11-02 18:15:14 +0200

    added "factlist" command

--- handlers.py
+++ handlers.py
@@ -331,6 +331,23 @@ class Facts(Command):
                 text=result.format(*users)
             )
             return True
+        elif cmd == 'factlist':
+            results = db.select('SELECT text FROM facts')
+            if not results:
+                raise Exception('В базі ще немає жодного факту :(')
+            
+            engine.telegram.sendMessage(
+                chat_id=message.chat_id,
+                text='Всі факти:\n\n' + '\n'.join(
+                    [
+                        '@{}'.format(row[0])
+                        for row
+                        in results
+                        ]
+                ),
+                parse_mode='Markdown'
+            )
+            return True
         elif cmd == 'addfact':
             if len(args.strip()) == 0:
                 raise Exception('Введіть, що саме додати! Наприклад: "/addfact {} любить фапати на аніме.".')
