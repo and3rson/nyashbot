96d0411ce5f67a95b19cee963b49ee9a117943fb
and3rson <anderson.dunai@gmail.com>
2015-11-06 07:06:47 +0200

    Fixed typo.

--- handlers.py
+++ handlers.py
@@ -268,7 +268,7 @@ class Stats(Command):
 
     def handle_version(self, engine, message, cmd, args):
         engine.telegram.sendMessage(
-            chad_it=message.chat_id,
+            chad_id=message.chat_id,
             text=self.get_version(),
             parse_mode='Markdown'
         )
