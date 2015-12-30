e5a304bac3d6db45f83e627804d2cb5bcd0a282e
and3rson <anderson.dunai@gmail.com>
2015-12-30 05:34:35 +0200

    Cleanup

--- query3.py
+++ query3.py
@@ -15,6 +15,9 @@ class Packet(object):
         del self.data[:n]
         return result
 
+    def can_read(self, n):
+        return len(self.data) >= n
+
     def readB(self):
         return ord(self.read(1)[0])
 
@@ -26,7 +29,7 @@ class Packet(object):
 
     def readS(self):
         length = self.readB()
-        return ''.join(self.read(length)[:-1])
+        return ''.join(self.read(length)[:-1]).replace('\xC2\xA0', ' ')
 
     skip = read
 
@@ -49,43 +52,27 @@ class Connection(object):
             return Packet(self.conn.recv(1024))
         raise Exception('UT2004 server {}:{} seems to be offline.'.format(self.host, self.port))
 
-    #def send(self, data):
-        #self.conn.sendto(data, (HOST, PORT))
-
-    #def recv(self):
-        #if self.conn in select.select([self.conn], [], [], TIMEOUT)[0]:
-            #data = self.conn.recv(1024)
-            #parts = filter(None, data.split('\\'))
-            #return dict(zip(parts[::2], parts[1::2]))
-        #raise Exception('Timeout while trying to request data.')
-
-    #def request(self, command):
-        #self.send('\\{}\\'.format(command))
-        #return self.recv()
-
     def get_info(self):
         self.send('\x7F\x00\x00\x00\x00')
         response = self.recv()
 
         response.skip(18)
 
-        return dict(
+        info = dict(
             host=self.host,
             port=self.port,
             server_name=response.readS(),
             map_name=response.readS(),
             game_type=response.readS(),
             player_count=response.readD(),
-            max_player_count=response.readD()
+            max_player_count=response.readD(),
+            ping=response.readD()
         )
 
-#try:
-#except Exception as e:
-    #print e.message
-    #sys.exit(1)
-#else:
-    #print 'Server: 5.187.7.185:{}'.format(info['hostport'])
-    # 'Current map: {}'.format(info['maptitle'])
-    #print 'Players: {}/{}'.format(info['numplayers'], info['maxplayers'])
-    #print 'Protocol: {}, min: {}'.format(info['gamever'], info['minnetver'])
-    #sys.exit(0)
+        if response.can_read(6):
+            info.update(dict(
+                flags=response.readD(),
+                skill=response.readW()
+            ))
+
+        return info
